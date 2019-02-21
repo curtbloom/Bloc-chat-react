df96d386577155f58cd1ab152ac9efadc1fdf109
Curtis Bloomfield <curtbloom@gmail.com>
2019-02-21 10:46:30 -0500

    room-list-user-story

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,7 +7,7 @@ class RoomList extends Component {
        this.state = {
   	 rooms: []
        };
-       this.roomsRef = this.props.firebase.database().ref('rooms');
+       this.roomsRef = this.props.firebase.database().ref("rooms");
 }
 
 componentDidMount() {
@@ -24,19 +24,15 @@ handleClick(index) {
 
   render() {
     return (
-      <div>
-        {this.state.rooms.map((room, index) => {
-          return (
-            <p
-              key={room.key}
-              onClick={() => this.handleClick(index)}
-            >
-              {room.val}
-            </p>
-          )
-        })}
-      </div>
-    )
+      <section className="room-list">
+          <h3>Rooms</h3>
+          {this.state.rooms.map( room =>
+               <li key={room.key} >
+                 {room.name}
+               </li>
+           )}
+       </section>
+    );
   }
 }
 
